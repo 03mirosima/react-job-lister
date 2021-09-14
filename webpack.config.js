c99423f94994bd664b057e71b2f3cbc87cea749d
03mirosima <sezenmiray2009@hotmail.com>
2021-09-14 13:40:56 +0300

    Added babel-loader and babel config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
 module.exports = {
-  mode: "development",
+  mode: mode,
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
   devServer: {
     static: {
-      directory: "./public",
+      directory: "./dist",
     },
   },
 };
